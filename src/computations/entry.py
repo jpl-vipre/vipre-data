924175750b9f7183fe94f67150665e591c73c7f9
Michael Fedell <mfedell@jpl.nasa.gov>
2022-04-07 12:20:36 -0700

    Add src dir for back-end computations

---
+++ src/computations/entry.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def bvec_theta() -> float:
+    """
+    Compute the B-plane theta vector for a single entry.
+
+    :return: Angle on B-plane for target arrival [rad]
+    """
+    pass
+
+
+def bvec_mag() -> float:
+    """
+    Compute the B-plane magnitude for a single entry
+
+    :return: Radial distance from body origin for target arrival [km]
+    """
+    pass
