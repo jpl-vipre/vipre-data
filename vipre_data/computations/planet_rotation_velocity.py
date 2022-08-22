7448e2a46fd85e7bc9a47e37b93cf1bf05d55f68
Alex Davis <Alex.Davis@jpl.nasa.gov>
2022-08-22 10:50:47 -0700

    Added function to calculate planetary rotation velocity
    Former-commit-id: ba24b733dbb598a2d331a1822e617fe9ea1aa8c5

---
+++ vipre_data/computations/planet_rotation_velocity.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy import linalg as la
+
+def planet_rotation_velocity(R,period):
+	# Compute planetary rotation velocity in body frame at specified position. Add this to V_entry to get relative velocity in planet atmosphere or on surface
+
+	# inputs:
+	#    R: position on planet, column vectors
+	#    period: rotational period in days
+
+	# outputs:
+	#    V: rotational velocity
+
+	w=np.tile(np.array([[0.],[0.],[2.*np.pi/period/86400.]]),(1,np.shape(R)[1]))#angular velocity
+	V=np.cross(R,w,axis=0)
+	return V
