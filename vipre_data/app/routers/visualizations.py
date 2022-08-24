17b47c5df64bb4a7f853aa9343420d44ea59c252
Michael Fedell <mfedell@jpl.nasa.gov>
2022-08-23 18:54:53 -0700

    feat: add func stubs for carrier arc and probe arc
    Former-commit-id: 3ca64a023436e21d584e22a3e822c57b939a0866

--- vipre_data/app/routers/visualizations.py
+++ vipre_data/app/routers/visualizations.py
@@ -8,8 +8,7 @@ from vipre_data.app.schemas.utils import get_xyz_tuple, make_lat_long
 from vipre_data.computations.cart2sph import cart2sph
 from vipre_data.sql import crud, models
 from vipre_data.computations.conic_2point import conic_2point
-
-# from vipre_data.computations.conic_1point import conic_1point
+from vipre_data.computations.conic_1point import conic_1point
 
 router = APIRouter(
     prefix="/visualizations",
@@ -28,10 +27,58 @@ def trajectory_selection(
     return result
 
 
+def get_carrier_arc(maneuver: models.Maneuver, final_time: int):
+    # TODO: Implement via conic_1point
+    params = dict(
+        r_1=get_xyz_tuple(maneuver, "pos_man"),
+        v_1=get_xyz_tuple(maneuver, "vel_man"),
+        t_1=np.array([[maneuver.time_man]]),
+        mu=maneuver.entry.target_body.mu,
+        time_flag=0,  # Likely not ever changed by user
+    )
+    pos_set, vel_set, time_set = conic_1point(**params)
+    # pos_set: [ [[ x1, x2 ]], [[ y1, y2 ]], [[ z1, z2 ]] ]
+    # vel_set: same...
+    # time_set: [ [[ t1, t2 ]] ]
+
+    # Identify last valid entry in time_set (all tuples up to final_time)
+    final_idx = 0
+    for i, time in enumerate(time_set.flatten()):
+        if time >= final_time:
+            final_idx = i
+            break
+
+    #
+    pos_set = pos_set[:, :, :final_idx]  # slice each component (x,y,z) up to final_idx
+    height, lat, long = cart2sph(*pos_set)
+
+    return height, lat, long
+
+
+def get_probe_arc(entry: models.Entry, maneuver: models.Maneuver, ta_step: int):
+    params = dict(
+        r_1=get_xyz_tuple(maneuver, "pos_man"),
+        r_2=get_xyz_tuple(entry, "pos_entry"),
+        v_1=get_xyz_tuple(maneuver, "vel_man"),
+        v_2=get_xyz_tuple(entry, "vel_entry"),
+        t_1=np.array([[maneuver.time_man]]),
+        t_2=np.array([[entry.t_entry]]),
+        mu=entry.target_body.mu,
+        ta_step=ta_step,  # Just needs to be high enough for a smooth arc
+        rev_check=0,  # Likely not ever changed by user
+        time_flag=0,  # Likely not ever changed by user
+    )
+    pos_set, vel_set, time_set = conic_2point(**params)
+
+    height, lat, long = cart2sph(*pos_set)
+    return height, lat, long
+
+
 @router.post("/get_entry_arc/{entry_id}", response_model=schemas.response.TrajectoryArcs)
 def get_trajectory_arc(
     entry_id: int, req: schemas.request.EntryArcRequest, db: Session = Depends(deps.get_db)
 ):
+    # Fetch required
     entry: models.Entry = db.query(models.Entry).where(models.Entry.id == entry_id).first()
     maneuver: models.Maneuver = (
         db.query(models.Maneuver)
@@ -39,27 +86,21 @@ def get_trajectory_arc(
         .where(models.Maneuver.maneuver_type == "divert")
         .first()
     )
-    params = dict(
-        r_1=get_xyz_tuple(maneuver, "pos_man"),
-        r_2=get_xyz_tuple(entry, "pos_entry"),
-        v_1=get_xyz_tuple(maneuver, "vel_man"),
-        v_2=get_xyz_tuple(entry, "vel_entry"),
-        t_1=np.array([[maneuver.time_man]]),
-        # t_1=np.array([[maneuver.time_man]]),
-        t_2=np.array([[entry.t_entry]]),
-        mu=entry.target_body.mu,
-        ta_step=req.ta_step,  # Just needs to be high enough for a smooth arc
-        rev_check=0,  # Likely not ever changed by user
-        time_flag=0,  # Likely not ever changed by user
+    final_datarate: models.Datarate = (
+        db.query(models.Datarate)
+        .where(models.Datarate.entry_id == entry.id)
+        .order_by(models.Datarate.order.desc())
+        .first()
     )
-    pos_set, vel_set, time_set = conic_2point(**params)
 
-    # TODO: Add carrier trajectory computed from conic_1point
-    # add dv_man to vel_man for conic1:v_1
+    # Generate arc for probe trajectory
+    height, lat, long = get_probe_arc(entry, maneuver, req.ta_step)
+    probe_lat_long = make_lat_long(height, lat, long)
 
-    height, lat, lon = cart2sph(*pos_set)
-    probe_lat_long = make_lat_long(height, lat, lon)
-    carrier_lat_long = []
+    # Generate arc for carrier trajectory
+    height, lat, long = get_carrier_arc(entry, final_datarate.time)
+    carrier_lat_long = make_lat_long(height, lat, long)
+
     return {"carrier": carrier_lat_long, "probe": probe_lat_long}
 
 
