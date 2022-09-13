7b38950a41fd5f7987e11a83cedd5123711e2ff8
Michael Fedell <mfedell@jpl.nasa.gov>
2022-09-13 15:30:57 -0700

    feat: database hotswap via get_engine dependency
    This is perhaps a bit crude, but instead of creating the engine
    statically in the database.py module, we now create it within the
    context of a dependency which is able to check os.environ every time it
    is called. Also crude, but issuing a change to the database just
    involves accepting a new uri from the user and setting the env var
    
    
    Former-commit-id: 5c8f8fdbc0a72114fd2dd6b24a423fa58f21a0e1

--- vipre_data/app/dependencies.py
+++ vipre_data/app/dependencies.py
@@ -1,9 +1,27 @@
-from vipre_data.sql.database import SessionLocal
+import os
+from pathlib import Path
 
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker, Session
+
+
+# from vipre_data.sql.database import SessionLocal
 
 # Dependency
-def get_db():
-    db = SessionLocal()
+def get_engine():
+    this_file = Path(__file__).absolute()
+    # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+    sample = f'sqlite:///{(this_file.parent / "E_S_test_subset2.db").absolute()}'
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", sample)
+
+    engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+    return engine
+
+
+def get_db(engine: Engine = Depends(get_engine)):
+    db = Session(autocommit=False, autoflush=False, bind=engine)
     try:
         yield db
     finally:
